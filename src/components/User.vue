b9c266c1da070beaf3c7f045f8b994a92295fa12
lee <1825103317>
2021-03-08 13:07:05 +0800

    lee

---
+++ src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>哈哈哈</div>
+</template>
+
+<script>
+export default {
+  created(){
+  //   this.$axios.get('/users/getUserInfo?id=1').then(res=>{
+  //   console.log(res)
+  // })
+    this.$axios.get('/apis/users/getUserInfo?id=1').then(res=>{
+      console.log(res.data)
+    })
+
+  }
+}
+</script>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,18 @@ import HelloWorld from '@/components/HelloWorld'
 import Table from '@/components/Table'
 import Pop from '@/components/Pop'
 import Tab from '@/components/Tab'
+import User from '@/components/User'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'User',
+      component: User
+    },
+    {
+      path: '/tab',
       name: 'Tab',
       component: Tab
     },
